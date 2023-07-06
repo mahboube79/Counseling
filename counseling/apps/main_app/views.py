4f1e4201816e11f7f4dbc65b58441e583b123bca
mahboub.j79@gmail.com <mahboub.j79@gmail.com>
2023-07-06 14:38:26 +0330

    add navbar and home page to mainTemplate and config media folder settings

--- counseling/apps/main_app/views.py
+++ counseling/apps/main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.conf import settings
+
+def media_admin(request):
+    return {'media_url':settings.MEDIA_URL,}
+
 
 class MainView(View):
     def get(self,request,*args,**kwargs):
--- counseling/counseling/settings.py
+++ counseling/counseling/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.main_app.apps.MainAppConfig',
+    # 'apps.acounting.apps.AcountingConfig',
+    
     
 ]
 
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.main_app.views.media_admin',
             ],
         },
     },
